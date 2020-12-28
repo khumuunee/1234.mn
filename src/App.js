dbb8f3cc59f72a96eba563dd1379c1b2add2db7b
khumuunee <76699571+khumuunee@users.noreply.github.com>
2020-12-29 01:08:10 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      khailtiinUtga: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((khariu) => khariu.json())
+      .then((urdun) => this.setState({ robots: urdun }));
+  }
+
+  khailtKhiiyee = (event) => {
+    this.setState({ khailtiinUtga: event.target.value });
+  };
+
+  render() {
+    const { robots, khailtiinUtga } = this.state;
+    const filteredRobots = robots.filter((el) =>
+      el.name.toLowerCase().includes(khailtiinUtga)
+    );
+
+    return (
+      <div className="App">
+        <h1>Роботуудын хайлт</h1>
+        <SearchBox khailtKhiiye={this.khailtKhiiyee} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "../card/card";
+
+import "./card-list.style.css";
+
+export const CardList = (props) => (
+  <div className="card-list">
+    {props.robots.map((el) => (
+      <Card key={el.id} robot={el} />
+    ))}
+  </div>
+);
